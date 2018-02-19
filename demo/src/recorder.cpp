fb4d3b2b8c9128f11867074cee21dcfe2638efb5
Abhinav Singh <theawless@gmail.com>
2018-02-20 02:19:54 +0530

    Update demos

--- demo/src/recorder.cpp
+++ demo/src/recorder.cpp
@@ -1,27 +1,52 @@
+#include <cmath>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "utils.h"
 
-#define DURATION 2
-#define N_UTTERANCES 15
-#define RECORD_FOLDER "B:\\record\\digit\\"
-#define RECORD_FILENAME RECORD_FOLDER "140101002"
-
 using namespace std;
 
-vector<string> audio_names = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+class Recorder
+{
+private:
+	const static int sample_rate = 16000;
+	const string record_command = "\"C:\\Program Files (x86)\\sox-14-4-2\\sox.exe\" -t waveaudio -c 1 -r 16000 -d -t s16 -q";
+
+public:
+	static void capture_audio(string filename, double duration)
+	{
+		string data_filename = filename + ".wav";
+		string recording_filename = filename + ".samples";
+		string record_command = record_command + " " + data_filename + " trim 0 " + to_string(duration);
+		cout << "Recording started, duration: " << duration << endl;
+		system(record_command.c_str());
+
+		fstream data(data_filename, ios::in | ios::binary);
+		vector<int16_t> amplitudes(ceil(sample_rate * duration));
+		data.read((char *)&amplitudes[0], amplitudes.size() * sizeof(amplitudes[0]));
+
+		Utils::set_vector_to_file<int16_t>(amplitudes, recording_filename);
+	}
+};
 
 /// The entry of the program, it takes user input, calls required functions and outputs the results.
-int main() {
+int main()
+{
+	const double duration = 0.8;
+	const int n_utterances = 15;
+	const string record_folder = "B:\\record\\";
+	const vector<string> audio_names = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
 	cout << "Starting recording session.";
-	for (int i = 1; i <= N_UTTERANCES; ++i) {
-		string utterance = pad_number(i, N_UTTERANCES);
-		cout << endl << "Utterances phase: " << utterance << endl;
-		for (int j = 0; j < audio_names.size(); ++j) {
+	for (int i = 1; i <= n_utterances; ++i)
+	{
+		cout << endl << "Utterances phase: " << i << endl;
+		for (int j = 0; j < audio_names.size(); ++j)
+		{
 			cout << endl << "Press enter to start for: " << audio_names[j] && cin.get();
-			capture_audio(string(RECORD_FILENAME) + "_" + audio_names[j] + "_" + utterance, DURATION);
+			Recorder::capture_audio(audio_names[j] + "_" + to_string(i), duration);
 		}
 	}
 	cout << endl << "All recordings done" << cin.get();
