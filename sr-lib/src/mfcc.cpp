d78307042f1b3ad55a354a832b29135d7ad8630e
Abhinav Singh <theawless@gmail.com>
2018-02-20 02:18:41 +0530

    Reorganise mfcc

--- sr-lib/src/mfcc.cpp
+++ sr-lib/src/mfcc.cpp
@@ -2,215 +2,225 @@
 
 #include <algorithm>
 #include <cmath>
-#include <complex>
-#include <map>
 #include <numeric>
-#include <vector>
 
 using namespace std;
 
-class MFCC {
+double MFCC::hertz2mel(double hz)
+{
+	return 2595 * log10(1 + hz / 700);
+}
 
-private:
-	const double PI = 4 * atan(1.0);
-	int fs;
-	map<int, std::map<int, complex<double>>> twiddle;
-	size_t numCepstra, numFFT, numFFTBins, numFilters;
-	double preEmphCoef, lowFreq, highFreq;
-	vector<double> frame, powerSpectralCoef, lmfbCoef, mfcc;
-	vector<vector<double>> fbank, dct;
+double MFCC::mel2hertz(double mel)
+{
+	return 700 * (pow(10, mel / 2595) - 1);
+}
 
-private:
-	inline double hz2mel(double f) {
-		return 2595 * std::log10(1 + f / 700);
+void MFCC::setup_twiddle()
+{
+	const complex<double> J(0, 1);
+
+	for (int N = 2; N <= n_fft; N *= 2)
+	{
+		for (int k = 0; k <= N / 2 - 1; ++k)
+		{
+			twiddle[N][k] = exp(-2 * PI * k / N * J);
+		}
+	}
+}
+
+void MFCC::setup_filterbank()
+{
+	double low_mel = hertz2mel(hz_low);
+	double high_mel = hertz2mel(hz_high);
+
+	// Calculate filter centre-frequencies
+	vector<double> hz_filter_center(n_filters + 2, 0.0);
+	for (int i = 0; i < n_filters + 2; ++i)
+	{
+		hz_filter_center[i] = mel2hertz(low_mel + (high_mel - low_mel) / (n_filters + 1) * i);
 	}
 
-	inline double mel2hz(double m) {
-		return 700 * (std::pow(10, m / 2595) - 1);
+	// Calculate FFT bin frequencies
+	vector<double> hz_fft_bin(n_fft_bins, 0.0);
+	for (int i = 0; i < n_fft_bins; ++i)
+	{
+		hz_fft_bin[i] = hz_sampling / 2.0 / (n_fft_bins - 1) * i;
 	}
 
-	// Twiddle factor computation
-	void compTwiddle(void) {
-		const complex<double> J(0, 1);
-		for (int N = 2; N <= numFFT; N *= 2) {
-			for (int k = 0; k <= N / 2 - 1; k++) {
-				twiddle[N][k] = exp(-2 * PI*k / N * J);
+	for (int filter = 1; filter <= n_filters; ++filter)
+	{
+		vector<double> row;
+		for (int bin = 0; bin < n_fft_bins; ++bin)
+		{
+			double weight;
+			if (hz_fft_bin[bin] < hz_filter_center[filter - 1])
+			{
+				weight = 0;
 			}
-		}
-	}
-
-	// Cooley-Tukey DIT-FFT recursive function
-	vector<complex<double>> fft(vector<complex<double>> x) {
-		int N = x.size();
-		if (N == 1) {
-			return x;
-		}
-
-		vector<complex<double>> xe(N / 2, 0), xo(N / 2, 0), Xjo, Xjo2;
-		int i;
-
-		// Construct arrays from even and odd indices
-		for (i = 0; i < N; i += 2) {
-			xe[i / 2] = x[i];
-		}
-		for (i = 1; i < N; i += 2) {
-			xo[(i - 1) / 2] = x[i];
-		}
-
-		// Compute N/2-point FFT
-		Xjo = fft(xe);
-		Xjo2 = fft(xo);
-		Xjo.insert(Xjo.end(), Xjo2.begin(), Xjo2.end());
-
-		// Butterfly computations
-		for (i = 0; i <= N / 2 - 1; i++) {
-			complex<double> t = Xjo[i], tw = twiddle[N][i];
-			Xjo[i] = t + tw * Xjo[i + N / 2];
-			Xjo[i + N / 2] = t - tw * Xjo[i + N / 2];
-		}
-		return Xjo;
-	}
-
-	// Power spectrum computation
-	void computePowerSpec(void) {
-		frame.resize(numFFT); // Pads zeros
-		vector<complex<double>> framec(frame.begin(), frame.end()); // Complex frame
-		vector<complex<double>> fftc = fft(framec);
-
-		for (int i = 0; i < numFFTBins; i++) {
-			powerSpectralCoef[i] = pow(abs(fftc[i]), 2);
-		}
-	}
-
-	// Applying log Mel filterbank (LMFB)
-	void applyLMFB(void) {
-		lmfbCoef.assign(numFilters, 0);
-
-		for (int i = 0; i < numFilters; i++) {
-			// Multiply the filterbank matrix
-			for (int j = 0; j < fbank[i].size(); j++) {
-				lmfbCoef[i] += fbank[i][j] * powerSpectralCoef[j];
+			else if (hz_fft_bin[bin] <= hz_filter_center[filter])
+			{
+				weight = (hz_fft_bin[bin] - hz_filter_center[filter - 1]) / (hz_filter_center[filter] - hz_filter_center[filter - 1]);
 			}
-			// Apply Mel-flooring
-			if (lmfbCoef[i] < 1.0) {
-				lmfbCoef[i] = 1.0;
+			else if (hz_fft_bin[bin] <= hz_filter_center[filter + 1])
+			{
+				weight = (hz_filter_center[filter + 1] - hz_fft_bin[bin]) / (hz_filter_center[filter + 1] - hz_filter_center[filter]);
 			}
-		}
-
-		// Applying log on amplitude
-		for (int i = 0; i < numFilters; i++) {
-			lmfbCoef[i] = std::log(lmfbCoef[i]);
-		}
-	}
-
-	// Computing discrete cosine transform
-	void applyDct(void) {
-		mfcc.assign(numCepstra + 1, 0);
-		for (int i = 0; i <= numCepstra; i++) {
-			for (int j = 0; j < numFilters; j++)
-				mfcc[i] += dct[i][j] * lmfbCoef[j];
-		}
-	}
-
-	// Initialisation routines pre-computing and dct matrix
-	void initDct(void) {
-		int i, j;
-
-		vector<double> v1(numCepstra + 1, 0), v2(numFilters, 0);
-		for (i = 0; i <= numCepstra; i++)
-			v1[i] = i;
-		for (i = 0; i < numFilters; i++)
-			v2[i] = i + 0.5;
-
-		dct.reserve(numFilters*(numCepstra + 1));
-		double c = sqrt(2.0 / numFilters);
-		for (i = 0; i <= numCepstra; i++) {
-			vector<double> dtemp;
-			for (j = 0; j < numFilters; j++)
-				dtemp.push_back(c * cos(PI / numFilters * v1[i] * v2[j]));
-			dct.push_back(dtemp);
-		}
-	}
-
-	// Precompute filterbank
-	void initFilterbank() {
-		// Convert low and high frequencies to Mel scale
-		double lowFreqMel = hz2mel(lowFreq);
-		double highFreqMel = hz2mel(highFreq);
-
-		// Calculate filter centre-frequencies
-		vector<double> filterCentreFreq;
-		filterCentreFreq.reserve(numFilters + 2);
-		for (int i = 0; i < numFilters + 2; i++)
-			filterCentreFreq.push_back(mel2hz(lowFreqMel + (highFreqMel - lowFreqMel) / (numFilters + 1)*i));
-
-		// Calculate FFT bin frequencies
-		vector<double> fftBinFreq;
-		fftBinFreq.reserve(numFFTBins);
-		for (int i = 0; i < numFFTBins; i++)
-			fftBinFreq.push_back(fs / 2.0 / (numFFTBins - 1)*i);
-
-		// Filterbank: Allocate memory
-		fbank.reserve(numFilters*numFFTBins);
-
-		// Populate the fbank matrix
-		for (int filt = 1; filt <= numFilters; filt++) {
-			vector<double> ftemp;
-			for (int bin = 0; bin < numFFTBins; bin++) {
-				double weight;
-				if (fftBinFreq[bin] < filterCentreFreq[filt - 1])
-					weight = 0;
-				else if (fftBinFreq[bin] <= filterCentreFreq[filt])
-					weight = (fftBinFreq[bin] - filterCentreFreq[filt - 1]) / (filterCentreFreq[filt] - filterCentreFreq[filt - 1]);
-				else if (fftBinFreq[bin] <= filterCentreFreq[filt + 1])
-					weight = (filterCentreFreq[filt + 1] - fftBinFreq[bin]) / (filterCentreFreq[filt + 1] - filterCentreFreq[filt]);
-				else
-					weight = 0;
-				ftemp.push_back(weight);
+			else
+			{
+				weight = 0;
 			}
-			fbank.push_back(ftemp);
+			row.push_back(weight);
+		}
+		filter_bank.push_back(row);
+	}
+}
+
+void MFCC::setup_dct()
+{
+	double c = sqrt(2.0 / n_filters);
+	vector<double> v1(n_cepstra + 1, 0.0), v2(n_filters, 0.0);
+
+	for (int i = 0; i <= n_cepstra; ++i)
+	{
+		v1[i] = i;
+	}
+	for (int i = 0; i < n_filters; ++i)
+	{
+		v2[i] = i + 0.5;
+	}
+	for (int i = 0; i <= n_cepstra; ++i)
+	{
+		vector<double> row;
+		for (int j = 0; j < n_filters; j++)
+		{
+			row.push_back(c * cos(PI / n_filters * v1[i] * v2[j]));
+		}
+		dct_matrix.push_back(row);
+	}
+}
+
+vector<complex<double>> MFCC::fft(const vector<complex<double>> &x)
+{
+	int N = x.size();
+	if (N == 1)
+	{
+		return x;
+	}
+	vector<complex<double>> xe(N / 2, 0.0), xo(N / 2, 0.0), Xjo, Xjo2;
+
+	// Construct arrays from even and odd indices
+	for (int i = 0; i < N; i += 2)
+	{
+		xe[i / 2] = x[i];
+	}
+	for (int i = 1; i < N; i += 2)
+	{
+		xo[(i - 1) / 2] = x[i];
+	}
+
+	// Compute N/2-point FFT
+	Xjo = fft(xe);
+	Xjo2 = fft(xo);
+	Xjo.insert(Xjo.end(), Xjo2.begin(), Xjo2.end());
+
+	// Butterfly computations
+	for (int i = 0; i <= N / 2 - 1; ++i)
+	{
+		complex<double> t = Xjo[i], tw = twiddle[N][i];
+		Xjo[i] = t + tw * Xjo[i + N / 2];
+		Xjo[i + N / 2] = t - tw * Xjo[i + N / 2];
+	}
+
+	return Xjo;
+}
+
+vector<double> MFCC::power_spectrum(const vector<double> &r_frame)
+{
+	vector<double> P(n_fft_bins, 0.0);
+	vector<complex<double>> frame(r_frame.begin(), r_frame.end());
+	frame.resize(n_fft, complex<double>(0, 0));
+	vector<complex<double>> transformed_frame = fft(frame);
+
+	for (int i = 0; i < n_fft_bins; ++i)
+	{
+		P[i] = pow(abs(transformed_frame[i]), 2);
+	}
+
+	return P;
+}
+
+vector<double> MFCC::lmfb(const vector<double> &P)
+{
+	vector<double> H(n_filters, 0.0);
+
+	for (int i = 0; i < n_filters; ++i)
+	{
+		for (int j = 0; j < filter_bank[i].size(); ++j)
+		{
+			H[i] += filter_bank[i][j] * P[j];
+		}
+		H[i] = max(H[i], 1.0);
+	}
+	for (int i = 0; i < n_filters; i++)
+	{
+		H[i] = log(H[i]);
+	}
+
+	return H;
+}
+
+void MFCC::normalise(vector<double> &C)
+{
+	double sum_amplitude = accumulate(C.begin(), C.end(), 0.0);
+	double mean_amplitude = sum_amplitude / C.size();
+
+	for (int i = 0; i < C.size(); ++i)
+	{
+		C[i] -= mean_amplitude;
+	}
+}
+
+vector<double> MFCC::dct(const vector<double> &H)
+{
+	vector<double> C(n_cepstra + 1, 0.0);
+
+	for (int i = 0; i <= n_cepstra; ++i)
+	{
+		for (int j = 0; j < n_filters; ++j)
+		{
+			C[i] += dct_matrix[i][j] * H[j];
 		}
 	}
 
-public:
-	// MFCC class constructor
-	MFCC(int sampFreq = 16000, int nCep = 12, int numFilt = 40, double lf = 50, double hf = 6500) {
-		fs = sampFreq;     // Sampling frequency
-		numCepstra = nCep;         // Number of cepstra
-		numFFT = 512;          // FFT size
-		numFilters = numFilt;      // Number of Mel warped filters
-		preEmphCoef = 0.97;         // Pre-emphasis coefficient
-		lowFreq = lf;           // Filterbank low frequency cutoff in Hertz
-		highFreq = hf;         // Filterbank high frequency cutoff in Hertz
+	return vector<double>(C.begin() + 1, C.end());
+}
 
-		numFFTBins = numFFT / 2 + 1;
-		powerSpectralCoef.assign(numFFTBins, 0);
+MFCC::MFCC()
+{
+	setup_twiddle();
+	setup_filterbank();
+	setup_dct();
+}
 
-		initFilterbank();
-		initDct();
-		compTwiddle();
+vector<double> MFCC::mfcc(const vector<double> &frame)
+{
+	vector<double> P = power_spectrum(frame);
+	vector<double> H = lmfb(P);
+	vector<double> C = dct(H);
+	normalise(C);
+
+	return C;
+}
+
+vector<vector<double>> MFCC::mfcc(const vector<vector<double>> &frames)
+{
+	vector<vector<double>> mfccs;
+
+	for (int i = 0; i < frames.size(); ++i)
+	{
+		mfccs.push_back(mfcc(frames[i]));
 	}
 
-	// Process each frame and extract MFCC
-	std::vector<double> processFrame(const vector<double> &samples, size_t N) {
-		frame = samples;
-		computePowerSpec();
-		applyLMFB();
-		applyDct();
-
-		return mfcc;
-	}
-};
-
-MFCC mfcc = MFCC();
-
-vector<vector<double>> speech_to_coefficients(const vector<vector<double>> &segments) {
-	vector<vector<double>> coefficients;
-
-	for (int i = 0; i < segments.size(); ++i) {
-		vector<double> C = mfcc.processFrame(segments[0], segments.size());
-		coefficients.push_back(C);
-	}
-
-	return coefficients;
+	return mfccs;
 }
