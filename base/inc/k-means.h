c880a3148b2e946e2e6e8ea0afd327a1a57f6da4
Abhinav Singh <theawless@gmail.com>
2018-03-18 11:38:27 +0530

    Use const and fix ordering

--- base/inc/k-means.h
+++ base/inc/k-means.h
@@ -6,20 +6,21 @@
 class KMeans
 {
 private:
-	const double convergence_threshold = 0.0000001;
-	const int convergence_max_iterations = 50;
+	static constexpr double convergence_threshold = 0.0000001;
+	static constexpr int convergence_max_iterations = 50;
+
 	const std::vector<std::vector<double>> &universe;
 
 	/// Locates centroids by taking means of vectors that have been put into respective buckets.
-	void relocate(const std::vector<int> &indices, std::vector<std::vector<double>> &centroids);
+	void relocate(const std::vector<int> &indices, std::vector<std::vector<double>> &centroids) const;
 
 public:
 	/// Constructor.
 	KMeans(const std::vector<std::vector<double>> &universe);
 
-	/// Classifies the universe into buckets.
-	std::pair<double, std::vector<int>> classify(const std::vector<std::vector<double>> &centroids);
-
 	/// Can be called directly with random initial centroids, or as a co-routine in LBG algorithm.
-	std::vector<std::vector<double>> optimize(const std::vector<std::vector<double>> &centroids);
+	std::vector<std::vector<double>> optimise(const std::vector<std::vector<double>> &centroids) const;
+
+	/// Classifies the universe into buckets.
+	std::pair<double, std::vector<int>> classify(const std::vector<std::vector<double>> &centroids) const;
 };
--- base/src/k-means.cpp
+++ base/src/k-means.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void KMeans::relocate(const vector<int> &indices, vector<vector<double>> &centroids)
+void KMeans::relocate(const vector<int> &indices, vector<vector<double>> &centroids) const
 {
 	vector<int> bucket_sizes(centroids.size(), 0);
 
@@ -33,7 +33,26 @@ KMeans::KMeans(const std::vector<std::vector<double>> &universe) : universe(univ
 {
 }
 
-pair<double, vector<int>> KMeans::classify(const std::vector<std::vector<double>> &centroids)
+vector<vector<double>> KMeans::optimise(const std::vector<std::vector<double>> &old_centroids) const
+{
+	int iteration = 0;
+	vector<vector<double>> centroids = old_centroids;
+	pair<double, vector<int>> buckets, new_buckets = classify(centroids);
+
+	do
+	{
+		buckets = new_buckets;
+		iteration += 1;
+		Logger::log("Kmeans: iteration:", iteration, "distortion is:", buckets.first);
+
+		relocate(buckets.second, centroids);
+		new_buckets = classify(centroids);
+	} while (buckets.first - new_buckets.first > convergence_threshold && iteration < convergence_max_iterations);
+
+	return centroids;
+}
+
+pair<double, vector<int>> KMeans::classify(const std::vector<std::vector<double>> &centroids) const
 {
 	double distortion = 0.0;
 	vector<int> indices(universe.size(), 0);
@@ -44,35 +63,16 @@ pair<double, vector<int>> KMeans::classify(const std::vector<std::vector<double>
 		int min_j = 0;
 		for (int j = 0; j < centroids.size(); ++j)
 		{
-			double distance = Maths::tokhura_distance(universe[i], centroids[j]);
+			const double distance = Maths::distance(universe[i], centroids[j]);
 			if (distance < min_distance)
 			{
 				min_distance = distance;
 				min_j = j;
 			}
 		}
-		indices[i] = min_j;
 		distortion += min_distance;
+		indices[i] = min_j;
 	}
 
 	return pair<double, vector<int>>(distortion, indices);
-}
-
-vector<vector<double>> KMeans::optimize(const std::vector<std::vector<double>> &old_centroids)
-{
-	int iteration = 0;
-	vector<vector<double>> centroids = old_centroids;
-	pair<double, vector<int>> buckets, new_buckets = classify(centroids);
-
-	do
-	{
-		buckets = new_buckets;
-		iteration += 1;
-		Logger::log("Kmeans: iteration:", iteration, ", distortion is:", buckets.first);
-
-		relocate(buckets.second, centroids);
-		new_buckets = classify(centroids);
-	} while (buckets.first - new_buckets.first > convergence_threshold && iteration < convergence_max_iterations);
-
-	return centroids;
 }
