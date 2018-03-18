b7fb306e8b1d197ab0da4ac100149dff130c8631
Abhinav Singh <theawless@gmail.com>
2018-03-18 11:42:25 +0530

    Refactor

--- base/inc/preprocess.h
+++ base/inc/preprocess.h
@@ -5,34 +5,35 @@
 class Preprocessor
 {
 private:
-	const int samples_per_frame;
-	const int samples_window_overlap;
-	const double normalisation_value = 5000.0;
-	const double pre_emphasis_factor = 0.95;
-	std::vector<double> hamming_coefficients;
+	static constexpr double normalisation_value = 5000.0;
+	static constexpr double pre_emphasis_factor = 0.95;
+
+	const int x_frames;
+	const int x_overlap;
+	const std::vector<double> hamming_coefficients;
 
 	/// Setup hamming coefficients.
-	void setup_hamming_window();
+	static std::vector<double> setup_hamming_coefficients(int x_frames);
 
 	/// Fix DC offset of the signal.
-	void dc_offset(std::vector<double> &samples);
+	void dc_offset(std::vector<double> &samples) const;
 
 	/// Normalise amplitudes of the signal.
-	void normalise(std::vector<double> &samples);
+	void normalise(std::vector<double> &samples) const;
 
 	/// Premphasize - boost the higher frequencies.
-	void pre_emphasize(std::vector<double> &samples);
-
-	/// Apply hamming window.
-	void hamming_window(std::vector<double> &frame);
+	void pre_emphasize(std::vector<double> &samples) const;
 
 	/// Divides the segments into frames.
-	std::vector<std::vector<double>> framing(const std::vector<double> &samples);
+	std::vector<std::vector<double>> framing(const std::vector<double> &samples) const;
+
+	/// Apply hamming window.
+	void hamming_window(std::vector<double> &frame) const;
 
 public:
 	/// Constructor.
-	Preprocessor(int samples_per_frame, int samples_window_overlap);
+	Preprocessor(int x_frames, int x_overlap);
 
 	/// Process the samples and return frames.
-	std::vector<std::vector<double>> process(const std::vector<double> &samples);
+	std::vector<std::vector<double>> process(const std::vector<double> &samples) const;
 };
--- base/src/preprocess.cpp
+++ base/src/preprocess.cpp
@@ -8,20 +8,22 @@
 
 using namespace std;
 
-void Preprocessor::setup_hamming_window()
+vector<double> Preprocessor::setup_hamming_coefficients(int x_frames)
 {
-	const double pi = 4.0 * atan(1.0);
+	vector<double> hamming_coefficients(x_frames, 0.54);
 
-	for (int i = 0; i < samples_per_frame; ++i)
+	const double pi = 4.0 * atan(1.0);
+	for (int i = 0; i < x_frames; ++i)
 	{
-		hamming_coefficients[i] = 0.54 - (0.46 * cos((2 * pi * i) / (samples_per_frame - 1)));
+		hamming_coefficients[i] -= 0.46 * cos(2 * i  * pi / (x_frames - 1));
 	}
+
+	return hamming_coefficients;
 }
 
-void Preprocessor::dc_offset(vector<double> &samples)
+void Preprocessor::dc_offset(vector<double> &samples) const
 {
-	double sum_amplitude = accumulate(samples.begin(), samples.end(), 0.0);
-	double mean_amplitude = sum_amplitude / samples.size();
+	const double mean_amplitude = accumulate(samples.begin(), samples.end(), 0.0) / samples.size();
 
 	for (int i = 0; i < samples.size(); ++i)
 	{
@@ -29,10 +31,9 @@ void Preprocessor::dc_offset(vector<double> &samples)
 	}
 }
 
-void Preprocessor::normalise(vector<double> &samples)
+void Preprocessor::normalise(vector<double> &samples) const
 {
-	double max_amplitude = Maths::maximum_absolute(samples);
-	double normalisation_factor = normalisation_value / max_amplitude;
+	const double normalisation_factor = normalisation_value / Maths::maximum_absolute(samples);
 
 	for (int i = 0; i < samples.size(); ++i)
 	{
@@ -40,7 +41,7 @@ void Preprocessor::normalise(vector<double> &samples)
 	}
 }
 
-void Preprocessor::pre_emphasize(vector<double> &samples)
+void Preprocessor::pre_emphasize(vector<double> &samples) const
 {
 	for (int i = 1; i < samples.size(); ++i)
 	{
@@ -48,47 +49,41 @@ void Preprocessor::pre_emphasize(vector<double> &samples)
 	}
 }
 
-void Preprocessor::hamming_window(vector<double> &frame)
-{
-	for (int i = 0; i < frame.size(); ++i)
-	{
-		frame[i] *= hamming_coefficients[i];
-	}
-}
-
-vector<vector<double>> Preprocessor::framing(const vector<double> &samples)
+vector<vector<double>> Preprocessor::framing(const vector<double> &samples) const
 {
 	vector<vector<double>> frames;
 
-	for (int i = 0; i <= samples.size() - samples_per_frame; i += samples_window_overlap)
+	for (int i = 0; i <= samples.size() - x_frames; i += x_overlap)
 	{
-		vector<double>::const_iterator left = samples.begin() + i;
-		vector<double>::const_iterator right = left + samples_per_frame;
+		const vector<double>::const_iterator left = samples.begin() + i;
+		const vector<double>::const_iterator right = left + x_frames;
 		vector<double> frame(left, right);
+		hamming_window(frame);
 		frames.push_back(frame);
 	}
 
 	return frames;
 }
 
-Preprocessor::Preprocessor(int samples_per_frame, int samples_window_overlap) : samples_per_frame(samples_per_frame),
-samples_window_overlap(samples_window_overlap), hamming_coefficients(samples_per_frame)
+void Preprocessor::hamming_window(vector<double> &frame) const
 {
-	setup_hamming_window();
+	for (int i = 0; i < x_frames; ++i)
+	{
+		frame[i] *= hamming_coefficients[i];
+	}
 }
 
-vector<vector<double>> Preprocessor::process(const vector<double> &unprocessed_samples)
+Preprocessor::Preprocessor(int x_frames, int x_overlap) : x_frames(x_frames), x_overlap(x_overlap), hamming_coefficients(setup_hamming_coefficients(x_frames))
 {
-	vector<double> samples(unprocessed_samples);
+}
+
+vector<vector<double>> Preprocessor::process(const vector<double> &unprocessed_samples) const
+{
+	vector<double> samples = unprocessed_samples;
 
 	dc_offset(samples);
 	normalise(samples);
 	pre_emphasize(samples);
-	vector<vector<double>> frames = framing(samples);
-	for (int i = 0; i < frames.size(); ++i)
-	{
-		hamming_window(frames[i]);
-	}
 
-	return frames;
+	return framing(samples);
 }
