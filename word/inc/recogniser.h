c3f8af59992c3b88f1f85dfe3c288fd16327d556
Abhinav Singh <theawless@gmail.com>
2018-04-01 01:27:12 +0530

    Add recogniser

---
+++ word/inc/recogniser.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "config.h"
+#include "gram-tester.h"
+#include "model-tester.h"
+
+class Recogniser
+{
+public:
+	class Builder
+	{
+	public:
+		/// Constructor.
+		Builder(const std::string &folder, const std::vector<std::string> &words, const std::vector<std::vector<std::string>> &sentences, const Config &config);
+
+		/// Build the Recogniser.
+		std::unique_ptr<Recogniser> build() const;
+
+	private:
+		const std::string folder;
+		const std::vector<std::string> words;
+		const std::vector<std::vector<std::string>> sentences;
+		const Config config;
+		const double gram_weight;
+	};
+
+	/// Recognise the word with previous context.
+	std::pair<bool, std::string> recognise(const std::string &filename);
+
+	/// Clear the context.
+	void reset();
+
+private:
+	const std::vector<std::string> words;
+	const std::vector<std::vector<std::string>> sentences;
+	const std::unique_ptr<ModelTester> model_tester;
+	const std::unique_ptr<GramTester> gram_tester;
+	const double gram_weight;
+	std::vector<std::string> context;
+
+	/// Constructor.
+	Recogniser(std::vector<std::string> words, std::vector<std::vector<std::string>> sentences, std::unique_ptr<ModelTester> model_tester, std::unique_ptr<GramTester> gram_tester, double gram_weight);
+};
---
+++ word/src/recogniser.cpp
@@ -0,0 +1,66 @@
+#include "recogniser.h"
+
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+Recogniser::Builder::Builder(const string &folder, const vector<string> &words, const vector<vector<string>> &sentences, const Config &config) :
+	folder(folder), words(words), sentences(sentences), config(config),
+	gram_weight(config.get_val<double>("gram_weight", 0.5))
+{
+}
+
+unique_ptr<Recogniser> Recogniser::Builder::build() const
+{
+	return unique_ptr<Recogniser>(new Recogniser(words, sentences, ModelTester::Builder(folder, config).build(), GramTester::Builder(folder, config).build(), gram_weight));
+}
+
+pair<bool, string> Recogniser::recognise(const string &filename)
+{
+	pair<bool, string> word(false, string());
+
+	const pair<bool, vector<double>> model_scores = model_tester->test(filename);
+	if (!model_scores.first || *max_element(model_scores.second.begin(), model_scores.second.end()) == 0.0)
+	{
+		return word;
+	}
+
+	double best_mixed_score = numeric_limits<double>::max();
+	string best_word;
+	for (int i = 0; i < words.size(); ++i)
+	{
+		const pair<bool, double> gram_score = gram_tester->test(context, words[i]);
+		if (!gram_score.first || gram_score.second == 0.0)
+		{
+			continue;
+		}
+
+		double mixed_score = gram_weight * gram_score.second + model_scores.second[i];
+		if (best_mixed_score < mixed_score)
+		{
+			best_mixed_score = mixed_score;
+			best_word = words[i];
+		}
+	}
+	word.first = !best_word.empty();
+	word.second = best_word;
+
+	if (word.first)
+	{
+		context.push_back(word.second);
+	}
+
+	return word;
+}
+
+void Recogniser::reset()
+{
+	context.clear();
+}
+
+Recogniser::Recogniser(vector<string> words, vector<vector<string>> sentences, unique_ptr<ModelTester> model_tester, unique_ptr<GramTester> gram_tester, double gram_weight) :
+	words(words), sentences(sentences),
+	model_tester(move(model_tester)), gram_tester(move(gram_tester)), gram_weight(gram_weight), context()
+{
+}
