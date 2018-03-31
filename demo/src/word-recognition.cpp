6156e5ac051d0bcd46bea14a13b97ffaf9339d32
Abhinav Singh <theawless@gmail.com>
2018-04-01 01:28:08 +0530

    Simplify recognition demo

---
+++ demo/src/word-recognition.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "config.h"
+#include "logger.h"
+#include "model-tester.h"
+#include "model-trainer.h"
+#include "utils.h"
+
+using namespace std;
+
+int main()
+{
+	const string folder = "res/";
+
+	const string config_filename = folder + "sr-lib.config";
+	const string words_filename = folder + "sr-lib.words";
+	const Config config = Utils::get_item_from_file<Config>(config_filename);
+	const vector<string> words = Utils::get_vector_from_file<string>(words_filename);
+
+	Logger::info("Training...");
+	const string train_folder = folder + "train/";
+	const unique_ptr<ModelTrainer> model_trainer = ModelTrainer::Builder(train_folder, words, config).build();
+	const unique_ptr<ModelTester> model_tester = ModelTester::Builder(train_folder, config).build();
+
+	const string test_folder = folder + "test/";
+	vector<int> n_utterances(words.size(), 0), n_hits(words.size(), 0), n_errs(words.size(), 0);
+	for (int i = 0; i < words.size(); ++i)
+	{
+		for (int j = 0; ; ++j)
+		{
+			const string test_filename = test_folder + words[i] + '_' + to_string(j);
+			const string test_wav_filename = test_filename + ".wav";
+			if (!ifstream(test_wav_filename).good())
+			{
+				break;
+			}
+
+			const pair<bool, vector<double>> scores = model_tester->test(test_filename);
+			const int word_index = max_element(scores.second.begin(), scores.second.end()) - scores.second.begin();
+
+			n_utterances[i]++;
+			if (!scores.first || scores.second[word_index] == 0.0)
+			{
+				n_errs[i]++;
+			}
+			else if (word_index == i)
+			{
+				n_hits[i]++;
+			}
+		}
+	}
+
+	const int total_utterances = accumulate(n_utterances.begin(), n_utterances.end(), 0);
+	const int total_hits = accumulate(n_hits.begin(), n_hits.end(), 0);
+	const int total_errs = accumulate(n_errs.begin(), n_errs.end(), 0);
+	Logger::info("Hit percent is:", (total_hits * 100.0) / total_utterances);
+	Logger::info("Err percent is:", (total_errs * 100.0) / total_utterances);
+	cin.get();
+}
