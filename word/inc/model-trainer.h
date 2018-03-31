0d868dcc6b81aa7e793a750d8e45958051ebf514
Abhinav Singh <theawless@gmail.com>
2018-04-01 01:26:01 +0530

    Update model trainer

---
+++ word/inc/model-trainer.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "codebook.h"
+#include "config.h"
+#include "features.h"
+#include "model.h"
+#include "preprocess.h"
+#include "threads.h"
+
+class ModelTrainer
+{
+public:
+	class Builder
+	{
+	public:
+		/// Constructor.
+		Builder(const std::string &folder, const std::vector<std::string> &words, const Config &config);
+
+		/// Build the ModelTrainer.
+		std::unique_ptr<ModelTrainer> build() const;
+
+	private:
+		const std::string folder;
+		const std::vector<std::string> words;
+		const int n_thread;
+		const int x_frame;
+		const int x_overlap;
+		const std::string cepstral;
+		const int n_cepstra;
+		const int n_predict;
+		const bool q_gain;
+		const bool q_delta;
+		const bool q_accel;
+		const int x_codebook;
+		const int n_state;
+		const int n_bakis;
+		const int n_retrain;
+
+		/// Initialise cepstral.
+		std::unique_ptr<ICepstral> get_cepstral() const;
+	};
+
+	/// Train.
+	void train() const;
+
+private:
+	static constexpr char wav_ext[] = ".wav";
+	static constexpr char features_ext[] = ".features";
+	static constexpr char universe_ext[] = "sr-lib.universe";
+	static constexpr char codebook_ext[] = "sr-lib.codebook";
+	static constexpr char observations_ext[] = ".observations";
+	static constexpr char model_ext[] = ".model";
+
+	const std::string folder;
+	const std::vector<std::string> words;
+	const std::unique_ptr<ThreadPool> thread_pool;
+	const Preprocessor preprocessor;
+	const std::unique_ptr<ICepstral> cepstral;
+	const LBG lbg;
+	const Model::Builder model_builder;
+	const int n_retrain;
+
+	/// Constructor.
+	ModelTrainer(std::string folder, std::vector<std::string> words, std::unique_ptr<ThreadPool> thread_pool, Preprocessor preprocessor, std::unique_ptr<ICepstral> cepstral, LBG lbg, Model::Builder model_builder, int n_retrain);
+
+	/// Build the codebook using lbg.
+	Codebook get_codebook() const;
+
+	/// Build the universe by accumulating features from all words.
+	std::vector<Feature> get_universe() const;
+
+	/// Build the word universe by accumulating features from all utterances.
+	std::vector<Feature> get_word_universe(const std::string &filename) const;
+
+	/// Load and preprocess the samples, and return their features.
+	std::vector<Feature> get_features(const std::string &filename) const;
+
+	/// Get the model for given word index.
+	Model get_word_model(int word_index, const Codebook &codebook) const;
+
+	/// Optimise the train model using the observations from utterances of the given filename.
+	Model get_utterance_model(const std::string &filename, const Codebook &codebook, const Model &train_model) const;
+
+	/// Get the observations sequence from the codebook.
+	std::vector<int> get_observations(const std::string &filename, const Codebook &codebook) const;
+};
---
+++ word/src/model-trainer.cpp
@@ -0,0 +1,238 @@
+#include "model-trainer.h"
+
+#include <future>
+
+#include "hmm.h"
+#include "logger.h"
+#include "lpc.h"
+#include "mfc.h"
+#include "utils.h"
+#include "wav.h"
+
+using namespace std;
+
+ModelTrainer::Builder::Builder(const string &folder, const vector<string> &words, const Config &config) :
+	folder(folder), words(words), n_thread(config.get_val<int>("n_thread", 4 * thread::hardware_concurrency())),
+	x_frame(config.get_val<int>("x_frame", 300)), x_overlap(config.get_val<int>("x_overlap", 80)),
+	cepstral(config.get_val<string>("cepstral", "mfc")), n_cepstra(config.get_val<int>("n_cepstra", 12)), n_predict(config.get_val<int>("n_predict", 12)),
+	q_gain(config.get_val<bool>("q_gain", false)), q_delta(config.get_val<bool>("q_delta", false)), q_accel(config.get_val<bool>("q_accel", false)),
+	x_codebook(config.get_val<int>("x_codebook", 16)),
+	n_state(config.get_val<int>("n_state", 5)), n_bakis(config.get_val<int>("n_bakis", 1)), n_retrain(config.get_val<int>("n_retrain", 3))
+{
+}
+
+unique_ptr<ModelTrainer> ModelTrainer::Builder::build() const
+{
+	return unique_ptr<ModelTrainer>(new ModelTrainer(folder, words, unique_ptr<ThreadPool>(new ThreadPool(n_thread)), Preprocessor(x_frame, x_overlap), get_cepstral(), LBG(x_codebook), Model::Builder(n_state, x_codebook, n_bakis), n_retrain));
+}
+
+unique_ptr<ICepstral> ModelTrainer::Builder::get_cepstral() const
+{
+	unique_ptr<ICepstral> icepstal;
+
+	if (cepstral == "lpc")
+	{
+		icepstal.reset(new LPC(n_cepstra, q_gain, q_delta, q_accel, n_predict));
+	}
+	else if (cepstral == "mfc")
+	{
+		icepstal.reset(new MFC(n_cepstra, q_gain, q_delta, q_accel));
+	}
+
+	return icepstal;
+}
+
+void ModelTrainer::train() const
+{
+	const Codebook codebook = get_codebook();
+
+	vector<future<Model>> model_futures;
+	for (int i = 0; i < words.size(); ++i)
+	{
+		model_futures.push_back(thread_pool->enqueue(&ModelTrainer::get_word_model, this, i, codebook));
+	}
+	for (int i = 0; i < words.size(); ++i)
+	{
+		model_futures[i].get();
+	}
+}
+
+ModelTrainer::ModelTrainer(string folder, vector<string> words, unique_ptr<ThreadPool> thread_pool, Preprocessor preprocessor, unique_ptr<ICepstral> cepstral, LBG lbg, Model::Builder model_builder, int n_retrain) :
+	folder(folder), words(words), thread_pool(move(thread_pool)),
+	preprocessor(preprocessor), cepstral(move(cepstral)), lbg(lbg), model_builder(model_builder), n_retrain(n_retrain)
+{
+	train();
+}
+
+Codebook ModelTrainer::get_codebook() const
+{
+	Codebook codebook;
+	const string codebook_filename = folder + codebook_ext;
+	Logger::log("Getting", codebook_filename);
+
+	codebook = Utils::get_item_from_file<Codebook>(codebook_filename);
+	if (!codebook.empty())
+	{
+		return codebook;
+	}
+
+	const vector<Feature> universe = get_universe();
+	codebook = lbg.generate(universe);
+	Utils::set_item_to_file<Codebook>(codebook, codebook_filename);
+
+	return codebook;
+}
+
+vector<Feature> ModelTrainer::get_universe() const
+{
+	vector<Feature> universe;
+	const string universe_filename = folder + universe_ext;
+	Logger::log("Getting", universe_filename);
+
+	universe = Utils::get_vector_from_file<Feature>(universe_filename, '\n');
+	if (!universe.empty())
+	{
+		return universe;
+	}
+
+	vector<future<vector<Feature>>> word_universe_futures;
+	for (int i = 0; i < words.size(); ++i)
+	{
+		const string word_filename = folder + words[i];
+		word_universe_futures.push_back(thread_pool->enqueue(&ModelTrainer::get_word_universe, this, word_filename));
+	}
+	for (int i = 0; i < words.size(); ++i)
+	{
+		const vector<Feature> word_universe = word_universe_futures[i].get();
+		universe.insert(universe.end(), word_universe.begin(), word_universe.end());
+	}
+
+	Utils::set_vector_to_file<Feature>(universe, universe_filename, '\n');
+
+	return universe;
+}
+
+vector<Feature> ModelTrainer::get_word_universe(const string &filename) const
+{
+	vector<Feature> word_universe;
+
+	for (int i = 0; ; ++i)
+	{
+		const string utterance_filename = filename + "_" + to_string(i);
+		const vector<Feature> features = get_features(utterance_filename);
+		if (features.empty())
+		{
+			break;
+		}
+
+		word_universe.insert(word_universe.end(), features.begin(), features.end());
+	}
+
+	return word_universe;
+}
+
+vector<Feature> ModelTrainer::get_features(const string &filename) const
+{
+	vector<Feature> features;
+	const string features_filename = filename + features_ext;
+	Logger::log("Getting", features_filename);
+
+	features = Utils::get_vector_from_file<Feature>(features_filename);
+	if (!features.empty())
+	{
+		return features;
+	}
+
+	const string wav_filename = filename + wav_ext;
+	const Wav wav_file(wav_filename);
+	const vector<double> samples = wav_file.samples<double>();
+	if (samples.empty())
+	{
+		return features;
+	}
+
+	const vector<vector<double>> frames = preprocessor.process(samples);
+	features = cepstral->features(frames);
+	Utils::set_vector_to_file<Feature>(features, features_filename);
+
+	return features;
+}
+
+Model ModelTrainer::get_word_model(int word_index, const Codebook &codebook) const
+{
+	Model model;
+	const string model_filename = folder + to_string(word_index) + model_ext;
+	Logger::log("Getting", model_filename);
+
+	model = Utils::get_item_from_file<Model>(model_filename);
+	if (!model.empty())
+	{
+		return model;
+	}
+
+	model = model_builder.bakis();
+	for (int j = 0; j < n_retrain; ++j)
+	{
+		Model train_model;
+		const string filename = folder + words[word_index];
+		const string train_model_filename = filename + model_ext + "_" + to_string(j);
+		Logger::log("Getting", train_model_filename);
+
+		train_model = Utils::get_item_from_file<Model>(train_model_filename);
+		if (!train_model.empty())
+		{
+			model = train_model;
+			continue;
+		}
+
+		train_model = get_utterance_model(filename, codebook, model);
+		Utils::set_item_to_file<Model>(train_model, train_model_filename);
+		model = train_model;
+	}
+	Utils::set_item_to_file<Model>(model, model_filename);
+
+	return model;
+}
+
+Model ModelTrainer::get_utterance_model(const string &filename, const Codebook &codebook, const Model &train_model) const
+{
+	vector<Model> utterance_models;
+
+	for (int i = 0; ; ++i)
+	{
+		const string utterance_filename = filename + "_" + to_string(i);
+		const vector<int> observations = get_observations(utterance_filename, codebook);
+		if (observations.empty())
+		{
+			break;
+		}
+
+		utterance_models.push_back(HMM(train_model).optimise(observations));
+	}
+
+	return model_builder.merge(utterance_models);
+}
+
+vector<int> ModelTrainer::get_observations(const string &filename, const Codebook &codebook) const
+{
+	vector<int> observations;
+	const string obs_filename = filename + observations_ext;
+	Logger::log("Getting", obs_filename);
+
+	observations = Utils::get_vector_from_file<int>(obs_filename);
+	if (!observations.empty())
+	{
+		return observations;
+	}
+
+	const vector<Feature> features = get_features(filename);
+	if (features.empty())
+	{
+		return observations;
+	}
+
+	observations = codebook.observations(features);
+	Utils::set_vector_to_file<int>(observations, obs_filename);
+
+	return observations;
+}
